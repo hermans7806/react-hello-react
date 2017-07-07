aabdafcc10a701aaae1ae2717cca39db290be91d
Herman Sutanto <herman.sutanto@reebonz.com>
2017-07-07 17:54:14 +0800

    install package, setup webpack, and seperate components

---
+++ public/components/Greeter.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var GreeterMessage = require('./GreeterMessage.jsx');
+var GreeterForm = require('./GreeterForm.jsx');
+
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is default message'
+    };
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    };
+  },
+  handleNewData: function (updates) {
+    this.setState(updates);
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.state.message;
+
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData={this.handleNewData}/>
+      </div>
+    );
+  }
+});
+
+module.exports = Greeter;
